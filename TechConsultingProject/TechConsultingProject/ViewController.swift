ef2595b24d1454a5d8ee014354e4405385b84d57
uchegodfrey <ujay2002@yahoo.com>
2023-08-24 20:36:18 -0400

    Calculator screen

---
+++ TechConsultingProject/TechConsultingProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechConsultingProject
+//
+//  Created by Uche Godfrey on 2023-08-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
