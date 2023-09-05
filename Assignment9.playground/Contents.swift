a48799ddbe929ed39cb2ca6c71559537cec8b1e2
uchegodfrey <ujay2002@yahoo.com>
2023-09-05 19:20:31 -0400

    Assignment9

---
+++ Assignment9.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+// 
+
+
+//1. In Countires code, separate the netowork layer to different class.
+//
+//1.1 communicate from network Manager to Controller using Protocol Delegate
+//
+//1.1 communicate from network Manager to Controller using Escaping closure
+
+// In both 1.1 and 1.2 along with Countireis if api fails pass error back to view and show error in UI.
+
+
+
+
+// what is diff b/w escaping and non escaping -> escaping closure are function that stay in memorial beyond the scope of the calling function while non espcaping a closure that deleted from memory once the calling function is returned.
+// what is associated values -> non constant values attached to enum values
+// what is raw values -> constant values attached to enum values
+// what is Generics -> is feature that allows the creation of non specific type of classes, methods,functions and properties
+
+
+
