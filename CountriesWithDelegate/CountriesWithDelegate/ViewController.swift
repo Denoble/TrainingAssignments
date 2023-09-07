b70fa8b60b41430a9eaee688ace937e08dea6529
uchegodfrey <ujay2002@yahoo.com>
2023-09-07 09:50:41 -0400

    Adds Search Bar to Tableview

--- CountriesWithDelegate/CountriesWithDelegate/ViewController.swift
+++ CountriesWithDelegate/CountriesWithDelegate/ViewController.swift
@@ -7,29 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource,NetworkManagerActions {
+class ViewController: UIViewController,NetworkManagerActions{
+    
+    
     private var countries: [Country] = []
     private let networkManager = NetworkManager()
   
+    @IBOutlet var searchBar: UISearchBar!
     @IBOutlet var tableView: UITableView!
+    var filteredCountries = [Country]()
+    var searching:Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.tableView.dataSource = self
         networkManager.delegate  = self
+        self.searchBar.delegate = self
         networkManager.getCountries()
     }
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.countries.count
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell =  tableView.dequeueReusableCell(withIdentifier: "countryCell", for: indexPath) as? CountryTableViewCell ?? CountryTableViewCell()
-        cell.country.text = self.countries[indexPath.item].name
-        cell.capital.text = self.countries[indexPath.item].capital
-        cell.currency.text = self.countries[indexPath.item].currency.code
-        return cell
-    }
+    
     
     func refresh(countries: [Country]) {
         self.countries = countries
@@ -50,5 +47,47 @@ class ViewController: UIViewController, UITableViewDataSource,NetworkManagerActi
         
     }
     
+    func populateView(cell:CountryTableViewCell,dataSource:[Country],index:Int){
+        cell.country.text = dataSource[index].name
+        cell.capital.text = dataSource[index].capital
+        cell.currency.text = dataSource[index].currency.code
+    }
+    
+}
+
+extension ViewController : UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if(!searching){
+            return self.countries.count
+        }else{
+            return self.filteredCountries.count
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "countryCell", for: indexPath) as? CountryTableViewCell ?? CountryTableViewCell()
+        if(!searching){
+            populateView(cell: cell, dataSource:self.countries, index: indexPath.row)
+        }else{
+            populateView(cell: cell, dataSource:self.filteredCountries, index: indexPath.row)
+        }
+        /*cell.country.text = self.countries[indexPath.item].name
+        cell.capital.text = self.countries[indexPath.item].capital
+        cell.currency.text = self.countries[indexPath.item].currency.code*/
+        return cell
+    }
+}
+extension ViewController : UISearchBarDelegate{
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+          
+        if(searchText.isEmpty){
+            filteredCountries = countries
+        }else{
+            filteredCountries = countries.filter{$0.name.contains(searchText)}
+        }
+        searching = true
+        tableView.reloadData()
+    }
 }
 
